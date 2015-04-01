0b00385a27f58c763ddc9723018dd15194e6e902
xxUbivatelxx <mayerva@yandex.ru>
2015-04-01 15:17:23 +0600

    jakfjskgyk

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -5,10 +5,21 @@
 *     Дата создания:
 */
 
-void setup(){
-
+#include <LiquidCrystal.h>
+ 
+LiquidCrystal lcd(4, 5, 10, 11, 12, 13);
+ 
+void setup() 
+{
+    lcd.begin(16, 2);
+    lcd.print("Me\xBD\xC7 \xB7oByT");
+    lcd.setCursor(0, 1);
+    lcd.print("xxUBIVATORxx");
 }
+ 
+char symbol = 0;
 
-void loop(){
+void loop() 
+{
 
 }
